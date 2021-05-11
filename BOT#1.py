500905b206b0939ea12ca495ad0c0bb8552896f1
SolyanQueen <64539547+SolyanQueen@users.noreply.github.com>
2021-05-11 14:12:48 +0300

    Add files via upload

---
+++ BOT#1.py
@@ -0,0 +1,6 @@
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import Updater, messagehandler, Filters, CommandHandler
+token = '1684008354:AAH-Dt34-C40zR3wo5IjHbzNd8bsQfjL5Wg'
+
+def main():
+    
